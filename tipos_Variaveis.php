4728b2ae9ed14f1b7dd7a380dd22be97f44c0818
Franicsco <contatofranciscoassis@gmail.com>
2015-03-22 23:04:02 -0300

    Tipos de Variaveis

---
+++ tipos_Variaveis.php
@@ -0,0 +1,20 @@
+<?php
+
+//Tipos de variaveis
+
+//Tipo STRING
+$minhaVar = "Texto";
+echo($minhaVar . '<br />'); // resultado: TEXTO
+
+//Tipo BOOLEAN
+$minhaVar = true;
+echo($minhaVar . '<br />'); // resultado: 1
+//Tipo BOOLEAN
+$minhaVar = false;
+echo($minhaVar . '<br />'); // resultado:  <Vazio>
+
+
+//Tipo FLOAT
+$minhaVar = 5.22;
+echo($minhaVar . '<br />'); // resultado: 5.22
+?>
