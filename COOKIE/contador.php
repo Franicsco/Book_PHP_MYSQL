97f7f5a062e0b7669e7d114373ebb694c6373800
Francisco <contatofranciscoassis@gmail.com>
2015-06-04 18:18:35 -0300

    Update contador.php

--- COOKIE/contador.php
+++ COOKIE/contador.php
@@ -2,12 +2,12 @@
 if(@ $_COOKIE['visita'] == ""){
 	
 	$visita = 1;
-	setcookie("visita", $visita, time() + 10);
+	setcookie("visita", $visita, time() + 20);
 }
 else{
 	$_COOKIE['visita']++;
 	$visita = $_COOKIE['visita'];
-	setcookie("visita", $visita, time() + 10);
+	setcookie("visita", $visita, time() + 20);
 }
 
 echo $visita;
