2ea944daf456d59b0ce21f2598aaf4ad6290e54a
Franicsco <contatofranciscoassis@gmail.com>
2015-06-07 17:54:51 -0300

    Alteracoes

--- SISTEMA_LOGIN/index.php
+++ SISTEMA_LOGIN/index.php
@@ -1,3 +1,7 @@
+<?php 
+include('escolhaMenu.php');
+?>
+
 <!DOCTYPE HTML>
 <html lang="pt-br">
   
@@ -9,7 +13,8 @@
     <script type="text/javascript" src="jquery/jquery.min.js"></script>
       
     <!--Importando a Biblioteca JQuery-->
-    <script type="text/javascript" src="js/validacao.js"></script>
+    <script type="text/javascript" src="js/validacaoAcessar.js"></script>
+    <script type="text/javascript" src="js/validacaoCadastrar.js"></script>
       
   <title>Acesse sua conta</title>
   </head>
@@ -19,13 +24,13 @@
       <!--MENU-->
       <div id="opcao">      		
            <div class="menuAcesse">
-           	   <a href="##">               
+           	   <a href="index.php?acesse=acessar">               
                		<div class="menuTexto">ACESSE</div>
                </a>
            </div>
           	
            <div class="menuCadastro">  
-           	   <a href="##">                    
+           	   <a href="index.php?acesse=cadastrar">                    
                		<div class="menuTexto">CADASTRE-SE</div>                    
                </a>
            </div>          	
@@ -33,20 +38,7 @@
        <!--MENU-->
        
        <!--ESCOLHA DO MENU-->
-       <div id="escolhaFormAcesse">
-       		<div class="formAcesse">
-            	<h1>Acesse sua conta</h1>
-                <form action="logado" method="post">
-                    <input type="email" value="" placeholder="E-mail:" autofocus /><br />
-                    <input type="password" value="" placeholder="Senha:" /><br />
-                    <input type="submit" value="Acessar" class="btAcessar"/>
-                </form>
-            </div>
-       </div>
-       
-       <div id="escolhaFormCadastrese">
-       </div>
-       </div>
+       <?php echo menu();?>
        <!--ESCOLHA DO MENU-->
        
     </div>
